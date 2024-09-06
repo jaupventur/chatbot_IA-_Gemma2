532e69cd64e0fca0fa9585e2000c1767bcf7d0db
Joao Pedro <127329098+jaupventur@users.noreply.github.com>
2024-09-06 06:35:22 -0300

    Update Main.java

--- BotComOllama/TeleBot (copy)/src/main/java/Main.java
+++ BotComOllama/TeleBot (copy)/src/main/java/Main.java
@@ -2,7 +2,7 @@ import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
 public class Main {
     public static void main(String[] args) {
-        String botToken = "7480460886:AAFZae1St9p0HPsc1YlG2dWTu76AIRBELa8";
+        String botToken = "insira seu token criado pelo BotFather no telegram";
         try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
             botsApplication.registerBot(botToken, new MyAmazingBot(botToken));
             System.out.println("MyAmazingBot successfully started!");
